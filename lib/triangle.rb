fd3f09505a33808e0c223e15aa794eabdd9b44a7
ctninman <89664157+ctninman@users.noreply.github.com>
2022-01-07 11:32:00 +0100

    commit tree a52f24e18754ebac1da7d8325e19e9f0e070f0b8

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,32 @@
 class Triangle
-  # write code here
+
+  attr_reader :side_a, :side_b, :side_c
+  
+  def initialize (side_a, side_b, side_c)
+    @side_a = side_a
+    @side_b = side_b
+    @side_c = side_c
+  end
+
+  def kind
+    if side_a == 0 || side_b == 0 || side_c == 0
+      raise TriangleError
+    elsif side_a + side_b < side_c || side_a + side_c < side_b || side_b + side_c < side_a
+      raise TriangleError
+    elsif side_a == side_b && side_a == side_c
+      :equilateral
+    elsif side_a == side_b && side_a != side_c || side_b == side_c && side_b != side_a || side_a == side_c && side_a != side_b
+      :isosceles
+    elsif side_a != side_b && side_a != side_c && side_b != side_c
+      :scalene
+    end
+    
+  end
+
+  class TriangleError < StandardError
+    def message 
+      "That is not a valid triangle."
+    end
+  end
+
 end
